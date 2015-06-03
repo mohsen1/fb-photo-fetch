d1555c5c28b2eb8289dc191eaa351d51c8a3eb63
Mohsen Azimi <msn.azi@gmail.com>
2015-06-02 22:43:32 -0700

    A lot of UI work

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const userHome = require('user-home');
 const fs = require('fs');
 const path = require('path');
 const ipc = require('ipc');
+const execute = require('./execute');
+const remote = require('remote');
+const Menu = remote.require('menu');
+const MenuItem = remote.require('menu-item');
+const clipboard = require('clipboard');
+
+const menu = new Menu();
 
 const downloadFolderName = 'Facebook Photos';
 
@@ -11,9 +18,14 @@ angular
   .module('FBPhotoFetch', ['ngMaterial'])
   .controller('AppCtrl', AppCtrl);
 
-function AppCtrl($scope) {
+function AppCtrl($scope, $interval) {
   $scope.tagged = true;
-  $scope.token = null;
+  $scope.token = '';
+  var token = null;
+  $scope.tab = 0;
+  $scope.downloading = false;
+  $scope.bufferProgress = 0;
+  $scope.progress = 0;
 
   if (fs.existsSync(path.join(userHome, 'Pictures'))) {
     $scope.dest = path.join(userHome, 'Pictures', downloadFolderName);
@@ -24,28 +36,52 @@ function AppCtrl($scope) {
   $scope.openFB = function() {
     ipc.sendSync('open-fb');
   };
+
+  $scope.next = function(token_) {
+    if (token_) { token = token_; }
+    $scope.tab++;
+  };
+
+  $scope.prev = function() {
+    $scope.tab--;
+  };
+
+  $scope.download = function() {
+
+    // execute(token, $scope.tagged, $scope.dest, finish, progress);
+
+    $scope.downloading = true;
+
+    $interval(function () {
+      $scope.bufferProgress += 1;
+    }, 1000);
+
+    function progress (percent) {
+      $scope.progress = percent;
+    }
+
+    function finish(err) {
+      if (!err) {
+        alert('Downloaded all your photos');
+      } else {
+        alert('There was an error downloading your photos.');
+      }
+    }
+  };
+
+
+  $scope.paste =  paste;
+
+  function paste() {
+    $scope.token = clipboard.readText();
+  };
+
+
+  menu.append(new MenuItem({ label: 'Paste', click: paste }));
+
+  window.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+    menu.popup(remote.getCurrentWindow());
+  }, false);
 }
 
-// var throttle = require('lodash.throttle');
-
-// var execute = require('./execute');
-
-// document.querySelector('#download').addEventListener('click', function () {
-//   var token = document.querySelector('#token').value;
-//   var dest = document.querySelector('#dest').files[0].path;
-//   var tagged = document.querySelector('#tagged').checked;
-
-//   execute(token, tagged, dest, finish, throttle(progress, 100));
-
-//   function progress (percent) {
-//     document.querySelector('progress').value = percent;
-//   }
-
-//   function finish(err) {
-//     if (!err) {
-//       alert('Downloaded all your photos');
-//     } else {
-//       alert('There was an error downloading your photos.');
-//     }
-//   }
-// });
